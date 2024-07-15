bbefc47c6fb6c3371b6f59aa4259aa347c5b210a
Nanda Su <nanda.rnlkwc@gmail.com>
2024-07-15 14:48:11 +0530

    Infinix--sub--fina--fix

--- app/src/main/java/com/example/asl_project/WordSearchLists.java
+++ app/src/main/java/com/example/asl_project/WordSearchLists.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class WordSearchLists extends AppCompatActivity {
     AppCompatButton btn_common_words,btn_fruits,btn_veggies,btn_family;
@@ -13,6 +14,7 @@ public class WordSearchLists extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_word_search_2);
 
         //Initializing the buttons
